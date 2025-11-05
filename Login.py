c49a920667fc6c0f05e6046d085b73f0f7297b60
pier-colab <pier@student.upi.edu>
2025-11-05 13:34:58 +0700

    Update Fitur Login

--- Login.py
+++ Login.py
@@ -1,1 +1,20 @@
 print("Hello, welcome to the login module!")
+
+def proseslog():
+    pwbenar = "Latihan"
+    maksimalcoba = 3
+
+    for percobaan in range(1, maksimalcoba + 1):
+        password = input("Masukkan Password: ")
+
+        if password == pwbenar:
+            print("Login berhasil! Selamat datang di sistem RPL UPI Cibiru.")
+            return True
+        else:
+            if percobaan < maksimalcoba:
+                print(f"Password salah. Percobaan ke-{percobaan} dari {maksimalcoba}. Silakan coba lagi.\n")
+            else:
+                print("Akses ditolak. Hubungi administrator.")
+                return False
+
+proseslog()
